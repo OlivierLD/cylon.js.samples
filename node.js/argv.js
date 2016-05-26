05d6ebfafb164e0a00531f82465a3f91d2b33bb2
Olivier LeDiouris <olivier.lediouris@gmail.com>
2016-05-26 10:27:02 -0700

    More node.js tgricks

--- node.js/argv.js
+++ node.js/argv.js
@@ -5,3 +5,25 @@ if (process.argv.length <= 2) {
 }
 var param = process.argv[2];
 console.log('Param: ' + param);
+
+// Env variables, reading.
+var someValue = process.env['SOME_VALUE'];
+console.log("Some value:" + someValue);
+
+var val = 255;
+console.log("val:", val, "= 0x" + val.toString(16).trim().toUpperCase() + "");
+
+var toHexString = function(num, len) {
+  return "0x" + lpad(num.toString(16).trim().toUpperCase(), (len !== undefined ? len : 4), '0');
+}
+
+var lpad = function(str, len, pad) {
+  var s = str;
+  while (s.length < len) {
+    s = (pad !== undefined ? pad : " ") + s;
+  }
+  return s;
+}
+
+val = Number.MAX_VALUE; // Wow!
+console.log("Val:" + val + " = ", toHexString(val, 8));
